61ad6ad87e92662028f11a583b9aeac5a9d28f29
senior_lancer <senior_lancer@protonmail.com>
2020-10-09 14:48:38 +0800

    add feature to get firebase cloud message

--- app/src/main/java/com/seniorlancer/fcm/FirebaseMessagingService.java
+++ app/src/main/java/com/seniorlancer/fcm/FirebaseMessagingService.java
@@ -3,13 +3,35 @@ package com.seniorlancer.fcm;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-public class FirebaseMessagingService extends Service {
-    @Nullable
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+    private final String TAG = "FCM_LOG";
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        handleMessage(remoteMessage);
+    }
+
+    private void handleMessage(RemoteMessage remoteMessage) {
+        String from = remoteMessage.getFrom();
+        Log.i(TAG, "Message from: " + from);
+
+        if(remoteMessage.getData().size() > 0) {
+            if(true) {
+                Log.i(TAG, "123456");
+            } else {
+                Log.i(TAG, "abcedf");
+            }
+        }
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
     }
 }
--- app/src/main/java/com/seniorlancer/fcm/MainActivity.java
+++ app/src/main/java/com/seniorlancer/fcm/MainActivity.java
@@ -3,34 +3,71 @@ package com.seniorlancer.fcm;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApi;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnRetrieveToken;
+    private final String TAG = "FCM_LOG";
+    public static final int MY_BACKGROUND_JOB = 0;
 
+    Button btnRetrieveToken;
+    EditText textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnRetrieveToken = findViewById(R.id.btn_retrieve_toke);
+        textView = findViewById(R.id.txt_token);
+
         btnRetrieveToken.setOnClickListener(this);
+//        scheduleJob(this);
+    }
+
+    public void runTimeEnableAuto() {
+        FirebaseMessaging.getInstance().setAutoInitEnabled(true);
+    }
+
+    public static void scheduleJob(Context context) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            JobScheduler js = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+            JobInfo job = new JobInfo.Builder(
+                    MY_BACKGROUND_JOB,
+                    new ComponentName(context, MainActivity.class))
+                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                    .setRequiresCharging(true)
+                    .build();
+            js.schedule(job);
+        }
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_retrieve_toke:
-                retrieveFirebaseToken();
+                if(checkGooglePlayServices()) {
+                    retrieveFirebaseToken();
+                } else {
+                    Log.w(TAG, "Device doesn't have google play services");
+                }
                 break;
             default:
                 break;
@@ -42,13 +79,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onComplete(@NonNull Task<InstanceIdResult> task) {
                 if(!task.isSuccessful()) {
-                    Log.e("FCM", "Get to get token");
+                    Log.e(TAG, "Get to get token");
                     return;
                 }
 
                 String token = task.getResult().getToken();
+                textView.setText(token);
                 Toast.makeText(getApplicationContext(), token, Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private boolean checkGooglePlayServices() {
+        int status = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+
+        if(status != ConnectionResult.SUCCESS) {
+            Log.e(TAG, "Error to be available to google api");
+            return false;
+        } else {
+            Log.i(TAG, "Google play services updated");
+            return true;
+        }
     }
 }
